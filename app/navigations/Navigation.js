38e574cc1f0c735695351ee019b654b8913ab4d6
Joaquin Suarez <joaquin8123@gmail.com>
2021-12-09 01:02:12 -0300

    Create Navigation.js

---
+++ app/navigations/Navigation.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from 'react-native-elements'
+import ShopsStack from "./ShopsStack";
+import FavoritesStack from "./FavoritesStack";
+import SearchStack from "./SearchStack";
+import AccountStack from "./AccountStack";
+
+const Tab = createBottomTabNavigator();
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="shops"
+        tabBarOptions={{
+          inactiveTintColor: "#646464",
+          activeTintColor: "#00a680",
+        }}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color }) => screenOptions(route, color),
+        })}
+      >
+        <Tab.Screen
+          name="shops"
+          component={ShopsStack}
+          options={{ title: "Tiendas" }}
+        />
+        <Tab.Screen
+          name="favorites"
+          component={FavoritesStack}
+          options={{ title: "Favoritos" }}
+        />
+        <Tab.Screen
+          name="search"
+          component={SearchStack}
+          options={{ title: "Buscar" }}
+        />
+        <Tab.Screen
+          name="account"
+          component={AccountStack}
+          options={{ title: "Perfil" }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function screenOptions(route, color) {
+  let iconName;
+
+  switch (route.name) {
+    case "shops":
+      iconName = "storefront-outline";
+      break;
+    case "favorites":
+      iconName = "heart-outline";
+      break;
+    case "search":
+      iconName = "magnify";
+      break;
+    case "account":
+      iconName = "home-outline";
+      break;
+    default:
+      break;
+  }
+  return (
+    <Icon type="material-community" name={iconName} size={22} color={color} />
+  );
+}
